31ee00e08b9cb115d50784f4a4aae63aaa120653
Jaison Pereira <jaison.ramos@klabin.com.br>
2021-08-18 11:13:12 -0300

    criacao projeto e commit inicial

---
+++ postgres-container.sh
@@ -0,0 +1,2 @@
+echo "upping container postgressql"
+sudo docker run -p 5432:5432 -v /tmp/database:/var/lib/postgresql/data -e POSTGRES_PASSWORD=1234 postgres
---
+++ src/main/java/com/systembook/booksystem/BookSystemApplication.java
@@ -0,0 +1,23 @@
+package com.systembook.booksystem;
+
+import com.systembook.booksystem.view.Menu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({
+    "com.systembook"})
+@EnableAutoConfiguration
+public class BookSystemApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(BookSystemApplication.class).headless(false).run(args);
+        Menu menu = context.getBean(Menu.class);
+        menu.setVisible(true);
+    }
+
+}
